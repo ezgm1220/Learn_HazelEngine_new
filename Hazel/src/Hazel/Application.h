2b0d364433d1b0b87b405d957b3ec341e8b739bd
ezgm1220 <2431510525@qq.com>
2023-08-15 18:10:23 +0800

    12_窗口事件

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Hazel {
@@ -11,8 +13,14 @@ namespace Hazel {
 	public:
 		Application();
 		virtual ~Application();
+
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		// 窗口关闭处理函数
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
--- Hazel/src/Platform/WindowsWindow.cpp
+++ Hazel/src/Platform/WindowsWindow.cpp
@@ -1,10 +1,19 @@
 #include "hzpch.h"
 #include "WindowsWindow.h"
 
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/MouseEvent.h"
 
 namespace Hazel {
 
 	static bool s_GLFWInitialized = false;
+
+	// 定义一个静态的用于错误回调函数的错误函数
+	static void GLFWErrorCallback(int error, const char* description) {
+
+		HZ_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+	}
 
 	Window* Window::Create(const WindowProps& props)
 	{
@@ -35,6 +44,9 @@ namespace Hazel {
 			int success = glfwInit();
 			HZ_CORE_ASSERT(success, "Could not intialize GLFW!");
 
+			// 设置GLFW错误回调函数
+			glfwSetErrorCallback(GLFWErrorCallback);
+
 			s_GLFWInitialized = true;
 		}
 
@@ -42,7 +54,102 @@ namespace Hazel {
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
+
+		// 设置 GLFW 的回调函数
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+		{
+			// 从窗口中获取用户指针,并将其转换成我们之前定义的格式(WindowData)
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			// 写入数据
+			data.Width = width;
+			data.Height = height;
+			// 接下来创建事件
+			WindowResizeEvent event(width, height);
+			data.EventCallback(event);
+				
+
+		});
+
+		// 窗口关闭事件
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			WindowCloseEvent event;
+			data.EventCallback(event);
+
+		});
+
+		// 按键事件
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			// 将GLFW的键盘码转换到Hazel平台的键盘码
+			switch (action) {
+				case GLFW_PRESS:
+				{
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_REPEAT:
+				{
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		// 鼠标点击事件
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			// 将GLFW的事件转换到Hazel中
+			switch (action) {
+				case GLFW_PRESS:
+				{
+					MouseButtonPressedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					MouseButtonReleasedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		// 鼠标滚轮事件
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset,double yOffset)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			
+			MouseScrolledEvent event((float)xOffset, (float)yOffset);
+			data.EventCallback(event);
+		});
+
+		// 鼠标移动事件
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xPos, double yPos)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			
+			MouseMovedEvent event((float)xPos, (float)yPos);
+			data.EventCallback(event);
+		});
+
 	}
+
+	
 
 	void WindowsWindow::Shutdown()
 	{
