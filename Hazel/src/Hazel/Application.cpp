476315a0440468f5c2d5303871608ec966b5b637
ezgm1220 <2431510525@qq.com>
2023-08-15 18:10:39 +0800

    12_窗口事件

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -6,7 +6,7 @@
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
-
+	
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));// 设置回调函数
