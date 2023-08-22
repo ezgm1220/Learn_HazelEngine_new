5b83ae566db7d21d0465c16f4abb5eaedc50db94
ezgm1220 <2431510525@qq.com>
2023-08-22 17:09:42 +0800

    33_渲染流和提交

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,7 +1,6 @@
 #include "hzpch.h"
 #include "Application.h"
-
-#include <glad/glad.h>
+#include "Hazel/Renderer/Renderer.h"
 
 
 namespace Hazel {
@@ -157,16 +156,19 @@ namespace Hazel {
 
 		while (m_Running) {
 
-			glClearColor(0.2,0.2,0.2,1);
-			glClear(GL_COLOR_BUFFER_BIT);
+			RenderCommand::SetClearColor({ 0.2,0.2,0.2,1 });
+			RenderCommand::Clear();
 
-			m_BlueShader->Bind();
-			m_SquareVA->Bind();
-			glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			Renderer::BeginScene();
+			{
+				m_BlueShader->Bind();
+				Renderer::Submit(m_SquareVA);
 
-			m_Shader->Bind();
-			m_VertexArray->Bind();
-			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+				m_Shader->Bind();
+				Renderer::Submit(m_VertexArray);
+
+			}
+			Renderer::EndScene();
 
 
 			for (Layer* layer : m_LayerStack)
