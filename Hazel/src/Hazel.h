10830247abb1ed49414ba94e3a0af22bf9b9cfcd
ezgm1220 <2431510525@qq.com>
2023-08-25 09:56:51 +0800

    51_开始渲染器

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -15,6 +15,7 @@
 
 //------------------Render----------------------
 #include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/Renderer2D.h"
 #include "Hazel/Renderer/RenderCommand.h"
 
 #include "Hazel/Renderer/Buffer.h"
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -22,4 +22,4 @@ private:
 	Hazel::Ref<Hazel::Shader> m_FlatColorShader;
 
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
-};
+}; 
