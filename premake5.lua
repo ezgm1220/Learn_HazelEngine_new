35755e488734d217d6403acd02b815e8d99b50ee
ezgm1220 <2431510525@qq.com>
2023-10-31 23:43:51 +0800

    更新了库依赖以及Premake配置文件

--- premake5.lua
+++ premake5.lua
@@ -1,55 +1,57 @@
-workspace "LearnEngine"		-- 指的是解决方案
-	
+workspace "Hazel"
 	architecture "x86_64"
+	startproject "Sandbox"
 
 	configurations
 	{
-		"Debug",	
+		"Debug",
 		"Release",
-		"Dist"	-- 发行版本,剔除日志等功能
+		"Dist"
 	}
 	
-require "export-compile-commands"
-
+	flags
+	{
+		"MultiProcessorCompile"
+	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
--- 添加GLFW的配置文件
+-- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
-IncludeDir["Imgui"] = "Hazel/vendor/imgui"
+IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 IncludeDir["glm"] = "Hazel/vendor/glm"
 IncludeDir["stb_image"] = "Hazel/vendor/stb_image"
 
-include "Hazel/Vendor/GLFW"
-include "Hazel/Vendor/Glad"
-include "Hazel/Vendor/imgui"
+group "Dependencies"
+	include "Hazel/vendor/GLFW"
+	include "Hazel/vendor/Glad"
+	include "Hazel/vendor/imgui"
 
+group ""
 
 project "Hazel"
-	location "Hazel" -- Hazel这个项目的地址
-	kind "StaticLib" -- 动态库/改成静态链接
-	language "C++"	 
+	location "Hazel"
+	kind "StaticLib"
+	language "C++"
 	cppdialect "C++17"
 	staticruntime "on"
 
-	targetdir("bin/" .. outputdir .. "/%{prj.name}")	-- 二进制文件输出目录
-	objdir("bin-int/" .. outputdir .. "/%{prj.name}")-- obj 文件输出目录
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	-- 预处理头文件:
 	pchheader "hzpch.h"
 	pchsource "Hazel/src/hzpch.cpp"
 
-	-- 包含文件列表
 	files
 	{
-		"%{prj.name}/src/**.h",	-- 两个星号将会递归搜索文件夹子文件
+		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp",
 		"%{prj.name}/vendor/stb_image/**.h",
 		"%{prj.name}/vendor/stb_image/**.cpp",
 		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl"
+		"%{prj.name}/vendor/glm/glm/**.inl",
 	}
 
 	defines
@@ -57,107 +59,92 @@ project "Hazel"
 		"_CRT_SECURE_NO_WARNINGS"
 	}
 
-	-- 附加包含目录
 	includedirs
 	{
 		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.Imgui}",
+		"%{IncludeDir.ImGui}",
 		"%{IncludeDir.glm}",
 		"%{IncludeDir.stb_image}"
 	}
 
-	-- 链接的东西
-	links
-	{
+	links 
+	{ 
 		"GLFW",
 		"Glad",
-		"Imgui",
+		"ImGui",
 		"opengl32.lib"
 	}
 
-	filter "system:windows"	-- 过滤器.直到下一个过滤器之间都是这个过滤器执行的内容
-		systemversion "latest"	--指定windows SDK版本
-		
+	filter "system:windows"
+		systemversion "latest"
+
 		defines
 		{
-			"HZ_PLATFORM_WINDOWS",
 			"HZ_BUILD_DLL",
-			"GLFW_INCLUDE_NONE",
-			"HZ_DEBUG"
+			"GLFW_INCLUDE_NONE"
 		}
-
-
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
 		runtime "Debug"
-		symbols "On"
+		symbols "on"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
+
 project "Sandbox"
-	location "Sandbox" -- Hazel这个项目的地址
-	kind "ConsoleApp" -- 动态库
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
 	cppdialect "C++17"
-	language "C++"	 
 	staticruntime "on"
 
-	targetdir("bin/" .. outputdir .. "/%{prj.name}")	-- 二进制文件输出目录
-	objdir("bin-int/" .. outputdir .. "/%{prj.name}")-- obj 文件输出目录
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	-- 包含文件列表
 	files
 	{
-		"%{prj.name}/src/**.h",	-- 两个星号将会递归搜索文件夹子文件
+		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp"
 	}
 
-	-- 附加包含目录
 	includedirs
 	{
 		"Hazel/vendor/spdlog/include",
 		"Hazel/src",
-		"%{IncludeDir.glm}",
-		"Hazel/vendor"
+		"Hazel/vendor",
+		"%{IncludeDir.glm}"
 	}
 
-	-- 将 Hazel 和 Sandbox 链接起来
 	links
 	{
 		"Hazel"
 	}
 
-	filter "system:windows"	-- 过滤器.直到下一个过滤器之间都是这个过滤器执行的内容
-		systemversion "latest"	--指定windows SDK版本
+	filter "system:windows"
+		systemversion "latest"
 		
-		defines
-		{
-			"HZ_PLATFORM_WINDOWS"
-		}
-
-		
-
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
 		runtime "Debug"
-		symbols "On"
+		symbols "on"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
