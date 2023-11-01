d116d60bb2a1b91a08430e8bd21eebb436cf6a4e
ezgm1220 <2431510525@qq.com>
2023-11-01 22:58:04 +0800

    62_批渲染_纹理

--- Hazel/src/Platform/OpenGL/OpenGLShader.h
+++ Hazel/src/Platform/OpenGL/OpenGLShader.h
@@ -18,6 +18,7 @@ namespace Hazel {
 		virtual void Unbind() const override;
 
 		virtual void SetInt(const std::string& name, int value) override;
+		virtual void SetIntArray(const std::string& name, int* values, uint32_t count) override;
 		virtual void SetFloat(const std::string& name, float value) override;
 		virtual void SetFloat3(const std::string& name, const glm::vec3& value) override;
 		virtual void SetFloat4(const std::string& name, const glm::vec4& value) override;
@@ -28,6 +29,7 @@ namespace Hazel {
 		void UploadUniformInt(const std::string& name, int value);
 
 		void UploadUniformFloat(const std::string& name, float value);
+		void UploadUniformIntArray(const std::string& name, int* values, uint32_t count);
 		void UploadUniformFloat2(const std::string& name, const glm::vec2& value);
 		void UploadUniformFloat3(const std::string& name, const glm::vec3& value);
 		void UploadUniformFloat4(const std::string& name, const glm::vec4& value);
