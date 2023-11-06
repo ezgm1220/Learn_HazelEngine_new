56329ad62e8142a1574bf9c5b95efa99bdc0b886
ezgm1220 <2431510525@qq.com>
2023-11-06 21:48:37 +0800

    78_引入了entt实体组件,创建了场景,绘制实体

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -47,6 +47,7 @@
 #define HZ_ENABLE_ASSERTS
 #endif
 
+// TODO: Make this macro able to take in no arguments except condition
 #ifdef HZ_ENABLE_ASSERTS
 #define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
