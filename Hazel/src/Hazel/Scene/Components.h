0c9bad57e69f8051970849eb514025dbe1e0ad6f
ezgm1220 <2431510525@qq.com>
2023-11-07 14:33:12 +0800

    83_使用虚函数代替之前的写法

--- Hazel/src/Hazel/Scene/Components.h
+++ Hazel/src/Hazel/Scene/Components.h
@@ -55,22 +55,16 @@ namespace Hazel {
 	{
 		ScriptableEntity* Instance = nullptr;
 
-		std::function<void()> InstantiateFunction;
-		std::function<void()> DestroyInstanceFunction;
+		// 声明两个函数指针
+		ScriptableEntity* (*InstantiateScript)();
+		void (*DestroyScript)(NativeScriptComponent*);
 
-		std::function<void(ScriptableEntity*)> OnCreateFunction;
-		std::function<void(ScriptableEntity*)> OnDestroyFunction;
-		std::function<void(ScriptableEntity*, Timestep)> OnUpdateFunction;
-
+		// 通过模板去定义这两个函数指针
 		template<typename T>
 		void Bind()
 		{
-			InstantiateFunction = [&]() { Instance = new T(); };
-			DestroyInstanceFunction = [&]() { delete (T*)Instance; Instance = nullptr; };
-
-			OnCreateFunction = [](ScriptableEntity* instance) { ((T*)instance)->OnCreate(); };
-			OnDestroyFunction = [](ScriptableEntity* instance) { ((T*)instance)->OnDestroy(); };
-			OnUpdateFunction = [](ScriptableEntity* instance, Timestep ts) { ((T*)instance)->OnUpdate(ts); };
+			InstantiateScript = []() { return static_cast<ScriptableEntity*>(new T()); };
+			DestroyScript = [](NativeScriptComponent* nsc) { delete nsc->Instance; nsc->Instance = nullptr; };
 		}
 	};
 }
--- Hazel/src/Hazel/Scene/ScriptableEntity.h
+++ Hazel/src/Hazel/Scene/ScriptableEntity.h
@@ -7,11 +7,18 @@ namespace Hazel {
 	class ScriptableEntity
 	{
 	public:
+		virtual ~ScriptableEntity() {}
+
 		template<typename T>
 		T& GetComponent()
 		{
 			return m_Entity.GetComponent<T>();
 		}
+	// 改用虚函数形式
+	protected:
+		virtual void OnCreate() {}
+		virtual void OnDestroy() {}
+		virtual void OnUpdate(Timestep ts) {}
 	private:
 		Entity m_Entity;
 		friend class Scene;
