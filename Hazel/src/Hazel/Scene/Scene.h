d131b878d5f1e43de6dc8167c19f76409aa74808
ezgm1220 <2431510525@qq.com>
2023-11-07 09:56:15 +0800

    81_ 增加了可修改相机视口大小的功能

--- Hazel/src/Hazel/Scene/Scene.h
+++ Hazel/src/Hazel/Scene/Scene.h
@@ -21,8 +21,10 @@ namespace Hazel {
 		entt::registry& Reg() { return m_Registry; }
 
 		void OnUpdate(Timestep ts);
+		void OnViewportResize(uint32_t width, uint32_t height);
 	private:
 		entt::registry m_Registry;
+		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 		friend class Entity;
 	};
