1830b8aefea3fcf253eb99c89bbd5f380655e657
ezgm1220 <2431510525@qq.com>
2023-11-08 12:20:07 +0800

    更改了一些配置

--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -11,17 +11,17 @@ namespace Hazel {
 	{
 		switch (type)
 		{
-			case ShaderDataType::Float:    return 4;
-			case ShaderDataType::Float2:   return 4 * 2;
-			case ShaderDataType::Float3:   return 4 * 3;
-			case ShaderDataType::Float4:   return 4 * 4;
-			case ShaderDataType::Mat3:     return 4 * 3 * 3;
-			case ShaderDataType::Mat4:     return 4 * 4 * 4;
-			case ShaderDataType::Int:      return 4;
-			case ShaderDataType::Int2:     return 4 * 2;
-			case ShaderDataType::Int3:     return 4 * 3;
-			case ShaderDataType::Int4:     return 4 * 4;
-			case ShaderDataType::Bool:     return 1;
+		case ShaderDataType::Float:    return 4;
+		case ShaderDataType::Float2:   return 4 * 2;
+		case ShaderDataType::Float3:   return 4 * 3;
+		case ShaderDataType::Float4:   return 4 * 4;
+		case ShaderDataType::Mat3:     return 4 * 3 * 3;
+		case ShaderDataType::Mat4:     return 4 * 4 * 4;
+		case ShaderDataType::Int:      return 4;
+		case ShaderDataType::Int2:     return 4 * 2;
+		case ShaderDataType::Int3:     return 4 * 3;
+		case ShaderDataType::Int4:     return 4 * 4;
+		case ShaderDataType::Bool:     return 1;
 		}
 
 		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
@@ -47,17 +47,17 @@ namespace Hazel {
 		{
 			switch (Type)
 			{
-				case ShaderDataType::Float:   return 1;
-				case ShaderDataType::Float2:  return 2;
-				case ShaderDataType::Float3:  return 3;
-				case ShaderDataType::Float4:  return 4;
-				case ShaderDataType::Mat3:    return 3; // 3* float3
-				case ShaderDataType::Mat4:    return 4; // 4* float4
-				case ShaderDataType::Int:     return 1;
-				case ShaderDataType::Int2:    return 2;
-				case ShaderDataType::Int3:    return 3;
-				case ShaderDataType::Int4:    return 4;
-				case ShaderDataType::Bool:    return 1;
+			case ShaderDataType::Float:   return 1;
+			case ShaderDataType::Float2:  return 2;
+			case ShaderDataType::Float3:  return 3;
+			case ShaderDataType::Float4:  return 4;
+			case ShaderDataType::Mat3:    return 3; // 3* float3
+			case ShaderDataType::Mat4:    return 4; // 4* float4
+			case ShaderDataType::Int:     return 1;
+			case ShaderDataType::Int2:    return 2;
+			case ShaderDataType::Int3:    return 3;
+			case ShaderDataType::Int4:    return 4;
+			case ShaderDataType::Bool:    return 1;
 			}
 
 			HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
@@ -70,7 +70,7 @@ namespace Hazel {
 	public:
 		BufferLayout() {}
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements)
+		BufferLayout(std::initializer_list<BufferElement> elements)
 			: m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
